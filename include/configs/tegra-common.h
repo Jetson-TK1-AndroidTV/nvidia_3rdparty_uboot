b1def0cd4d5db837222cc5448e589f5b02d5668e
Michael Scott <michael.scott@linaro.org>
2014-09-24 11:03:59 -0700

    tegra: raise CONFIG_SYS_CBSIZE from 512 to 1024 bytes
    Due to this change in common/cmd_pxe.c:
    http://git.denx.de/cgi-bin/gitweb.cgi?p=u-boot.git;a=commit;h=b1ba62d4ae970a2da00371670a2c909f139d0f80
    
    The resulting cmdline passed to the kernel was being truncated to the length
    of CONFIG_SYS_CBSIZE (512 bytes at the time).  This was causing boot errors
    on Jetson where the total length was 703 bytes (as set by Nvidia)
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>
    
    Conflicts:
    	include/configs/tegra-common.h

--- include/configs/tegra-common.h
+++ include/configs/tegra-common.h
@@ -90,6 +90,7 @@
  * Increasing the size of the IO buffer as default nfsargs size is more
  *  than 256 and so it is not possible to edit it
  */
+
 #define CONFIG_SYS_CBSIZE		(1024 * 2) /* Console I/O Buffer Size */
 /* Print Buffer Size */
 #define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
