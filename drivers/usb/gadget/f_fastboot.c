891a7ae829109f4bb485728d45f6b48648a1f741
Michael Scott <michael.scott@linaro.org>
2014-09-28 16:37:12 -0700

    fastboot: tegra124: Add tegra support for fastboot reboot additions
    Set correct bits in scratch0 when using "fastboot reboot-bootloader" and
    "fastboot reboot-recovery"
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>
    
    Conflicts:
    	drivers/usb/gadget/f_fastboot.c

--- drivers/usb/gadget/f_fastboot.c
+++ drivers/usb/gadget/f_fastboot.c
@@ -23,6 +23,11 @@
 #ifdef CONFIG_FASTBOOT_FLASH_MMC_DEV
 #include <fb_mmc.h>
 #endif
+#if defined(CONFIG_TEGRA124)
+#include <asm/io.h>
+#include <asm/arch-tegra/pmc.h>
+#include <android_image.h>
+#endif
 
 #define FASTBOOT_VERSION		"0.4"
 
@@ -334,13 +339,18 @@ int __weak fb_set_reboot_flag(void)
 
 static void cb_reboot(struct usb_ep *ep, struct usb_request *req)
 {
+#if defined(CONFIG_TEGRA124)
 	char *cmd = req->buf;
-	if (!strcmp_l1("reboot-bootloader", cmd)) {
-		if (fb_set_reboot_flag()) {
-			fastboot_tx_write_str("FAILCannot set reboot flag");
-			return;
-		}
+	struct pmc_ctlr *pmc = (struct pmc_ctlr *)NV_PA_PMC_BASE;
+	if (strncmp("-bootloader", cmd + 6, 11) == 0) {
+		printf("Setting reboot-bootloader!\n");
+		writel(readl(&pmc->pmc_scratch0) | (1 << 30), &pmc->pmc_scratch0);
 	}
+	else if (strncmp("-recovery", cmd + 6, 9) == 0) {
+		printf("Setting reboot-recovery!\n");
+		writel(readl(&pmc->pmc_scratch0) | (1 << 31), &pmc->pmc_scratch0);
+	}
+#endif
 	fastboot_func->in_req->complete = compl_do_reset;
 	fastboot_tx_write_str("OKAY");
 }
